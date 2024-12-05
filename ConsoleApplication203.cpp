18662e4a54bfc55c377aae73ed04de47c64e3de8
neonz <neonz@MSI>
2024-12-06 00:24:23 +0700

    Задание 2

--- ConsoleApplication203.cpp
+++ ConsoleApplication203.cpp
@@ -1,45 +1,52 @@
 ﻿#include <iostream>
 #include <string>
 #include <regex>
+
 using namespace std;
 
-struct TemperatureMeasurements {   //хранение информации об объекте
-    string data;
+// Структура для хранения информации о температурных измерениях
+struct TemperatureMeasurements {
+    string date;
     string place;
-    float temp;
+    float temperature;
 
     void print() const {
-
         setlocale(LC_ALL, "RU");
         cout << "TemperatureMeasurements: \n";
-        cout << "Дата: " << data << " ";
+        cout << "Дата: " << date << " ";
         cout << "Место: " << place << " ";
-        cout << "Температура: " << temp << " °C ";
-
+        cout << "Температура: " << temperature << " °C\n";
     }
 };
 
-int main() {
-
-    string input = "2005.07.01 Moscow -18.7";
+// Функция для извлечения данных из строки
+bool extractData(const string& input, TemperatureMeasurements& measurements) {
     regex pattern("([\\d]+[\\.][\\d]+[\\.][\\d\\n]+[^\\s])([\\s]+)([a-zA-Z]+)([\\s]+)([-+]?\[\\d.]+)");
     smatch match;
 
-    // Проверяем, соответствует ли строка шаблону
     if (regex_match(input, match, pattern)) {
-        // Извлекаем данные из строки
-        string data = match[1];
-        string place = match[3];
-        float temp = stof(match[5]);
-        // Создаем объект с извлеченными данными
-        TemperatureMeasurements obj{ data, place, temp };
-        // Выводим данные объекта
-        obj.print();
+        measurements.date = match[1];
+        measurements.place = match[3];
+        measurements.temperature = stof(match[5]);
+        return true;
+    }
+    return false;
+}
+
+// Функция для обработки входной строки и вывода результата
+void processInput(const string& input) {
+    TemperatureMeasurements measurements;
+
+    if (extractData(input, measurements)) {
+        measurements.print();
     }
     else {
-
         cerr << "Ошибка: Не тот формат строки.\n";
     }
+}
 
+int main() {
+    string input = "2017.11.16 London -6.1";
+    processInput(input);
     return 0;
 }
